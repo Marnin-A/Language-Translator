e7aa42a031557ea3142c79e4a50630cf8ae2428f
Marnin Audu <msmaudu@gmail.com>
2022-08-26 08:38:19 +0100

    Cleaned up react folder and added tranlate component

---
+++ src/components/translate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Translate() {
+  return (
+    <div>
+      <div className="app-header">
+        <h1 className="header">
+          Translate any popular books to a <i id="naija">Nigerian Language</i>
+        </h1>
+      </div>
+    </div>
+  );
+}
