1757cab320e19df443d95a3fc39936caad166c41
Marnin Audu <msmaudu@gmail.com>
2022-09-30 22:11:49 +0100

    first commit

--- src/components/translate.js
+++ src/components/translate.js
@@ -1,12 +1,26 @@
 import React from "react";
-
+import { Form, TextArea } from "semantic-ui-react";
 export default function Translate() {
   return (
     <div>
+      {/* App Header */}
       <div className="app-header">
         <h1 className="header">
           Translate any popular books to a <i id="naija">Nigerian Language</i>
         </h1>
+      </div>
+
+      {/* App Body */}
+      <div className="app-body">
+        <div>
+          <Form>
+            <Form.Field
+              control={TextArea}
+              label="Input the Book here"
+              placeholder="Enter text to be translated"
+            />
+          </Form>
+        </div>
       </div>
     </div>
   );
