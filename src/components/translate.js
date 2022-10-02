220b70dc66c642c141969832de390a750a4cb13b
Marnin Audu <msmaudu@gmail.com>
2022-10-02 17:13:04 +0100

    installed axios and setup for api integration

--- src/components/translate.js
+++ src/components/translate.js
@@ -1,6 +1,16 @@
-import React from "react";
-import { Form, TextArea } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Icon, Form, TextArea } from "semantic-ui-react";
+import axios from "axios";
+
 export default function Translate() {
+  const [LanguageList, useLanguageList] = setState([]);
+  const [inputText, setInputText] = useState("");
+  const [resultText, setResultText] = useState("");
+  const translateText = () => {
+    setResultText(inputText);
+  };
+
+  useEffect(() => {}, [input]);
   return (
     <div>
       {/* App Header */}
@@ -14,14 +24,44 @@ export default function Translate() {
       <div className="app-body">
         <div>
           <Form>
+            {/* Input Field */}
             <Form.Field
               control={TextArea}
-              label="Input the Book here"
+              // label="Input the Book here"
               placeholder="Enter text to be translated"
+              onChange={(e) => setInputText(e.target.value)}
             />
+            <br />
+            {/* Language selection */}
+            <select class="ui dropdown">
+              <option value="">Select Language</option>
+              <option value="1">English</option>
+              <option value="0">French</option>
+            </select>
+            <br />
+            <br />
+
+            {/* Output Field */}
+            <Form.Field
+              control={TextArea}
+              // label="Input the Book here"
+              placeholder="translated text"
+              value={resultText}
+            />
+            <button
+              onClick={translateText}
+              class="ui primary button"
+              size="large"
+            >
+              {" "}
+              <Icon name="translate" /> Translate
+            </button>
           </Form>
         </div>
       </div>
+      <footer className="footer">
+        P.S Book translation will be available later on
+      </footer>
     </div>
   );
 }
