bfcdf7921b0f09684e8b0824dece93265cafdfd5
Marnin Audu <msmaudu@gmail.com>
2022-10-03 21:48:47 +0100

    fully integrated api

--- src/components/translate.js
+++ src/components/translate.js
@@ -4,23 +4,53 @@ import axios from "axios";
 
 export default function Translate() {
   const [LanguageList, setLanguageList] = useState([]);
+  const [selectedLanguageKey, setLanguageKey] = useState("");
   const [inputText, setInputText] = useState("");
   const [resultText, setResultText] = useState("");
+  const [detectedLangKey, setLangKey] = useState("");
+
+  const getLanguageSource = () => {
+    // prettier-ignore
+    axios.post(`https://libretranslate.de/detect`, { 
+      q : inputText 
+    })
+    .then((response) => {
+          setLangKey(response.data[0].language);
+        });
+  };
   const translateText = () => {
     setResultText(inputText);
+    getLanguageSource();
+    // prettier-ignore
+    let data = {
+      q: inputText,
+      source: detectedLangKey,
+      target: selectedLanguageKey
+    };
+    axios.post(`https://libretranslate.de/translate`, data).then((response) => {
+      setResultText(response.data.translatedText);
+    });
+  };
+
+  const languageKey = (selectedLanguage) => {
+    setLanguageKey(selectedLanguage.target.value);
   };
 
   useEffect(() => {
     axios.get(`https://libretranslate.de/languages`).then((response) => {
       setLanguageList(response.data);
     });
-  }, []);
+    getLanguageSource();
+  }, [inputText]);
   return (
     <div>
       {/* App Header */}
       <div className="app-header">
-        <h1 className="header">
+        {/* <h1 className="header">
           Translate any popular books to a <i id="naija">Nigerian Language</i>
+        </h1> */}
+        <h1 className="header">
+          Translate text to any<i id="naija"> Language</i>
         </h1>
       </div>
 
@@ -36,11 +66,12 @@ export default function Translate() {
               onChange={(e) => setInputText(e.target.value)}
             />
             <br />
+
             {/* Language selection */}
-            <select class="ui dropdown">
+            <select class="ui dropdown" onChange={languageKey}>
               <option value="">Select Language</option>
               {LanguageList.map((language) => {
-                return <option>{language.name}</option>;
+                return <option value={language.code}>{language.name}</option>;
               })}
             </select>
             <br />
